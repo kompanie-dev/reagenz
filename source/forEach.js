c27c5862a639be90ae4d81c524a497daca6f654f
Fabian <fabian.heuber@squer.io>
2024-01-06 11:56:03 +0100

    add support for Map to forEach

--- source/forEach.js
+++ source/forEach.js
@@ -1,7 +1,21 @@
 /**
  * Iterates through the given array and generates an HTML string using the supplied function.
- * @param {Array} array The array which should be iterated through.
+ * @param {Object} collection An object like Array or Map which should be iterated through.
  * @param {Function} htmlFunction A function which generates the HTML for each element in the array.
  * @returns A string which contains the complete HTML for each element in the array.
  */
-export const forEach = (array, htmlFunction) => array.map(htmlFunction).join("");
+export const forEach = (collection, htmlFunction) => {
+    if (collection instanceof Map) {
+        return Array
+            .from(collection)
+            .map(([key, value]) => htmlFunction(value, key))
+            .join("");
+    }
+    if (Array.isArray(collection)) {
+        return collection
+            .map(htmlFunction)
+            .join("");
+    }
+
+    throw new Error("The supplied collection is not iterable");
+};
