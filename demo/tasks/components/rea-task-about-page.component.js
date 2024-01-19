74a192d245693f05a643f36eb56c8212f2d68903
Fabian <fabian.heuber@squer.io>
2024-01-19 18:01:07 +0100

    update about page: - remove global event shorthand - add non-existing dependencies and click handler for showcasing the error handling

--- demo/tasks/components/rea-task-about-page.component.js
+++ demo/tasks/components/rea-task-about-page.component.js
@@ -1,20 +1,16 @@
 import { Component } from "../../../index.js";
 
 export class ReaTaskAboutPageComponent extends Component {
-    #logger = this.dependencies.logger;
+    #logger = this.dependencies.notExistingDependency;
 
     render() {
         return /*html*/`
             <h1>About My Tasks App</h1>
-            <div>
+            <div $click="notExistingClickFunction">
                 Welcome to the simple Tasks App!
             </div>
             <div class="margin-top-small">
                 <a href="/#" class="a button">Back to home</a>
             </div>`;
-    }
-
-    $click() {
-        this.#logger.log('Easter Egg 🥚');
     }
 }
