7ff91176e39c1284d1e599d4afbbcf8743ae6c21
Fabian <kompanie.dev@outlook.com>
2024-10-02 21:56:35 +0200

    add eslint to template project

---
+++ template-project/eslint.config.js
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended,
+    {
+        files: [
+            "./**/*.js",
+        ],
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            }
+        },
+        rules: { }
+    }
+];
