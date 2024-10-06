d3fb6581cb1454d711bf4d6ef98ba8b063d2ad75
Fabian <kompanie.dev@outlook.com>
2024-10-06 10:02:41 +0200

    added test for executeSelectors

--- tests/unit/store.test.js
+++ tests/unit/store.test.js
@@ -5,15 +5,28 @@ describe("Store.constructor()", () => {
 	it("Should correctly assign the initial state", () => {
 		const expectedState = { count: 0, data: "Test" };
 		const testReducer = () => { };
-		const testMiddlewareA = (store, next) => {
-			next();
-		};
-		const testMiddlewareB = (store, next) => {
-			next();
-		};
 
-		const testStore = new Store(testReducer, expectedState, [testMiddlewareA, testMiddlewareB]);
+		const testStore = new Store(testReducer, expectedState, []);
 
 		assert.deepEqual(testStore.state, expectedState);
+	});
+});
+
+describe("Store.executeSelectors()", () => {
+	it("Should correctly execute selector function and assign the resulting data", () => {
+		const initialState = { entries: ["Apple", "Orange", "Peach"], searchTerm: "p" };
+		const testReducer = () => { };
+		const testStore = new Store(testReducer, initialState, []);
+		const searchEntries = (state) => state.entries.filter(item => item.toLowerCase().includes(state.searchTerm.toLowerCase()));
+		const selectorObject = {
+			entries: searchEntries
+		};
+		const expected = {
+			entries: ["Apple", "Peach"]
+		};
+
+		const actual = testStore.executeSelectors(selectorObject);
+
+		assert.deepEqual(actual, expected);
 	});
 });
