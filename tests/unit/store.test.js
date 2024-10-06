0cdb132de2ba777ad72519f2707356dc3ec0d7e0
Fabian <kompanie.dev@outlook.com>
2024-10-06 10:10:30 +0200

    added tests for select and insecureSelect

--- tests/unit/store.test.js
+++ tests/unit/store.test.js
@@ -13,7 +13,7 @@ describe("Store.constructor()", () => {
 });
 
 describe("Store.executeSelectors()", () => {
-	it("Should correctly execute selector function and assign the resulting data", () => {
+	it("Should correctly execute the selector function and assign the resulting data", () => {
 		const initialState = { entries: ["Apple", "Orange", "Peach"], searchTerm: "p" };
 		const testReducer = () => { };
 		const testStore = new Store(testReducer, initialState, []);
@@ -26,6 +26,35 @@ describe("Store.executeSelectors()", () => {
 		};
 
 		const actual = testStore.executeSelectors(selectorObject);
+
+		assert.deepEqual(actual, expected);
+	});
+});
+
+
+describe("Store.select()", () => {
+	it("Should correctly execute the selector function and return the escaped resulting data", () => {
+		const initialState = { entries: ["<h1>Apple</h1>", "<h1>Orange</h1>", "<h1>Peach</h1>"], searchTerm: "p" };
+		const testReducer = () => { };
+		const testStore = new Store(testReducer, initialState, []);
+		const searchEntries = (state) => state.entries.filter(item => item.toLowerCase().includes(state.searchTerm.toLowerCase()));
+		const expected = ["&lt;h1&gt;Apple&lt;/h1&gt;", "&lt;h1&gt;Peach&lt;/h1&gt;"];
+
+		const actual = testStore.select(searchEntries);
+
+		assert.deepEqual(actual, expected);
+	});
+});
+
+describe("Store.insecureSelect()", () => {
+	it("Should correctly execute the selector function and return the unescaped resulting data", () => {
+		const initialState = { entries: ["<h1>Apple</h1>", "<h1>Orange</h1>", "<h1>Peach</h1>"], searchTerm: "p" };
+		const testReducer = () => { };
+		const testStore = new Store(testReducer, initialState, []);
+		const searchEntries = (state) => state.entries.filter(item => item.toLowerCase().includes(state.searchTerm.toLowerCase()));
+		const expected = ["<h1>Apple</h1>", "<h1>Peach</h1>"];
+
+		const actual = testStore.insecureSelect(searchEntries);
 
 		assert.deepEqual(actual, expected);
 	});
