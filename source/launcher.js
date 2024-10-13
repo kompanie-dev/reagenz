6d6be110d99a3c665f9db8e2d5965fd5c030c766
Fabian <kompanie.dev@outlook.com>
2024-10-13 23:42:05 +0200

    Bumped project version

--- source/launcher.js
+++ source/launcher.js
@@ -9,7 +9,7 @@ export class Launcher {
 	 */
 	static startApp(appComponentClass, appContainerElement) {
 		const appComponent = new appComponentClass();
-		appComponent.setAttribute("framework", "@kompanie/reagenz@8.0.0");
+		appComponent.setAttribute("framework", "@kompanie/reagenz@8.1.0");
 		appContainerElement.append(appComponent);
 	}
 }
