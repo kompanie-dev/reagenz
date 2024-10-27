de278e3bab71fb7373dc21826433b9ec980a3b1f
Fabian <kompanie.dev@outlook.com>
2024-10-27 17:31:21 +0100

    Export ObjectComparator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ export { App } from "./source/app.js";
 export { Component } from "./source/component.js";
 export { HtmlEscaper } from "./source/htmlEscaper.js";
 export { Modal } from "./source/modal.js";
+export { ObjectComparator } from "./source/objectComparator.js";
 export { Store } from "./source/store.js";
 
 export { ForComponent } from "./source/forComponent.js";
