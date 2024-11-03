5800993f6a295a1fe0853f98cab936d0cac0ea60
Fabian <kompanie.dev@outlook.com>
2024-11-03 13:01:49 +0100

    Add test for useDependencies()

--- tests/browser/component.test.js
+++ tests/browser/component.test.js
@@ -1,26 +1,26 @@
 import { assert, describe, it } from "vitest";
 import { Component } from "../../source/component.js";
 
-class AttributeTestComponent extends Component {
-	attributeTypes = {
-		testArray: Array,
-		testBoolean: Boolean,
-		testNumber: Number,
-		testObject: Object,
-		testString: String
-	};
-
-	render() {
-		const { testArray, testBoolean, testNumber, testObject, testString } = this.useAttributes();
-
-		return `<div>${testArray.length}/${testBoolean}/${testNumber}/${testObject.prop}/${testString}</div>`;
-	}
-}
-
-customElements.define("attribute-test-component", AttributeTestComponent);
-
 describe("Component", () => {
 	it("useAttributes() should convert and return attributes correctly", () => {
+		class AttributeTestComponent extends Component {
+			attributeTypes = {
+				testArray: Array,
+				testBoolean: Boolean,
+				testNumber: Number,
+				testObject: Object,
+				testString: String
+			};
+
+			render() {
+				const { testArray, testBoolean, testNumber, testObject, testString } = this.useAttributes();
+
+				return `<div>${testArray.length}/${testBoolean}/${testNumber}/${testObject.prop}/${testString}</div>`;
+			}
+		}
+
+		customElements.define("attribute-test-component", AttributeTestComponent);
+
 		document.body.insertAdjacentHTML("afterend", /*html*/`
 			<attribute-test-component
 				testArray="[7]"
@@ -34,5 +34,26 @@ describe("Component", () => {
 		const element = document.querySelector("attribute-test-component");
 
 		assert.equal(element.innerHTML, "<div>1/true/5/abc/Hello</div>");
+	});
+
+	it("useDependencies() should return dependencies correctly", () => {
+		class DependencyTestComponent extends Component {
+			render() {
+				const { testDependency } = this.useDependencies();
+
+				return `<div>${testDependency.value}</div>`;
+			}
+		}
+
+		customElements.define("dependency-test-component", DependencyTestComponent);
+
+		DependencyTestComponent.prototype.dependencies = { testDependency: { value: "success" } };
+		document.body.insertAdjacentHTML("afterend", /*html*/`
+			<dependency-test-component></dependency-test-component>
+		`);
+
+		const element = document.querySelector("dependency-test-component");
+
+		assert.equal(element.innerHTML, "<div>success</div>");
 	});
 });
