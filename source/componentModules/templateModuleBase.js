f7d12f2e4b725916d421acc46c49d5fdb6e74f8c
Fabian <kompanie.dev@outlook.com>
2026-02-21 21:35:02 +0100

    Make ifs explicit

--- source/componentModules/templateModuleBase.js
+++ source/componentModules/templateModuleBase.js
@@ -2,8 +2,8 @@ export class TemplateModuleBase {
   run({ template }) {
     while (true) {
       const deepest = this.#findDeepestTemplate(template.content);
-
-      if (!deepest) {
+      
+      if (deepest === null) {
         break;
       }
 
@@ -16,7 +16,7 @@ export class TemplateModuleBase {
     let maxDepth = -1;
 
     const visit = (node, depth) => {
-      if (!node) {
+      if (node === null || node === undefined) {
         return;
       }
 
