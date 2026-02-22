12764c4ac385b8937a003839b515a2c8261a4aad
Fabian <kompanie.dev@outlook.com>
2026-02-22 21:07:31 +0100

    Simplify html

--- source/html.js
+++ source/html.js
@@ -7,21 +7,25 @@ const storeInDataRegistry = (idPrefix, value) => {
   return id;
 };
 
-const renderValue = (str, value) => {
-  if (/\s\.\w+\s*=\s*["']?$/.test(str)) {
-    return storeInDataRegistry("dataID", value);
-  }
-
-  if (/@\w+\s*=\s*["']?$/.test(str)) {
-    return storeInDataRegistry("eventID", value);
-  }
-
-  if (/\sfor\s*=\s*["']?$/.test(str)) {
-    return storeInDataRegistry("forID", value);
-  }
-
-  return (typeof value === "boolean" || typeof value === "number" || typeof value === "string") ? value : "";
-};
-
 export const html = (strings, ...values) =>
-  strings.map((s, i) => s + renderValue(s, values[i])).join("");
+  strings
+    .map((s, i) => {
+      const value = values[i];
+      let rendered;
+
+      if (/\s\.\w+\s*=\s*["']?$/.test(s)) {
+        rendered = storeInDataRegistry("dataID", value);
+      }
+      else if (/@\w+\s*=\s*["']?$/.test(s)) {
+        rendered = storeInDataRegistry("eventID", value);
+      }
+      else if (/\sfor\s*=\s*["']?$/.test(s)) {
+        rendered = storeInDataRegistry("forID", value);
+      }
+      else {
+        rendered = typeof value === "boolean" || typeof value === "number" || typeof value === "string" ? value : "";
+      }
+
+      return s + rendered;
+    })
+    .join("");
